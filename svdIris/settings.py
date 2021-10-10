0c8652c55f5673e39ce60106227056845bf4ff2a
Edgar Ceron <edgar.ceron@correounivalle.edu.co>
2021-10-10 18:01:39 -0500

    static

--- svdIris/settings.py
+++ svdIris/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('/opt','app','static')
+STATIC_ROOT = os.path.join('/opt','app')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
